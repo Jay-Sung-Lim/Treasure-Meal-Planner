4a6ec44e6cec34d73f6decc445cc3cb1c217dc83
Jaesung Lim <wotjd2732@gmail.com>
2023-04-29 11:09:19 -0400

    add async function

--- Backend/index.js
+++ Backend/index.js
@@ -1,12 +1,17 @@
 const { Configuration, OpenAIApi } = require('openai');
+const OPENAI_API_KEY = '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';
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
-const completion = await openai.createCompletion({
-  model: 'text-davinci-003',
-  prompt: 'Hello world',
-});
-console.log(completion.data.choices[0].text);
+async function apiCall() {
+  const completion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: 'Hello world',
+  });
+  console.log(completion.data.choices[0].text);
+}
+
+apiCall();
