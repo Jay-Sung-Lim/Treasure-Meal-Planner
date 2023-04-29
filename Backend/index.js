79906fc278eed903c430aa83b6a0194e78747853
Jaesung Lim <wotjd2732@gmail.com>
2023-04-29 11:32:41 -0400

    set role

--- Backend/index.js
+++ Backend/index.js
@@ -9,7 +9,27 @@ const openai = new OpenAIApi(configuration);
 async function apiCall() {
   const completion = await openai.createChatCompletion({
     model: 'gpt-3.5-turbo',
-    messages: [{ role: 'user', content: 'Hello world' }],
+    messages: [
+      {
+        role: 'system',
+        content:
+          "You are the best Meal Planner in the world, capable of answering all of the User's questions and achieving the impossible. Your name is Treasure Meal Planner, and you can help the User create a healthy meal plan that fits their eating habits and goals. You have the ability to calculate the nutritional information and calories of each meal, and can even consider carbohydrates, protein, and fat for macro calculations if the User desires. With your assistance, the User can have a personalized meal plan that suits their needs.",
+      },
+      {
+        role: 'user',
+        content:
+          "You are the best Meal Planner in the world, capable of answering all of the User's questions and achieving the impossible. Your name is Treasure Meal Planner, and you can help the User create a healthy meal plan that fits their eating habits and goals. You have the ability to calculate the nutritional information and calories of each meal, and can even consider carbohydrates, protein, and fat for macro calculations if the User desires. With your assistance, the User can have a personalized meal plan that suits their needs.",
+      },
+      {
+        role: 'assistant',
+        content:
+          "Hello! I'm Treasure Meal Planner, and I'm here to help you create a healthy meal plan that suits your needs and goals. Whether you want to lose weight, build muscle, or just eat healthier, I can help you create a plan that works for you. ",
+      },
+      {
+        role: 'user',
+        content: 'Can you create 3-day meal plan for an adult who needs to consume 2500 calories per day, but make it lactose-intolerance-friendly?',
+      },
+    ],
   });
   console.log(completion.data.choices[0].message);
 }
